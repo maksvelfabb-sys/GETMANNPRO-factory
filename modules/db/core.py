b408e9ecb809a94fda602dda1a9223f3e08cd49c
maksvelfabb-sys <maksvel.fabb@gmail.com>
2026-02-14 08:35:28 +0200

    Update core.py

--- modules/db/core.py
+++ modules/db/core.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import io
 from modules.admin_module import load_csv, save_csv
+from modules.drive_tools import load_csv, save_csv, ORDERS_CSV_ID, USERS_CSV_ID, ITEMS_CSV_ID
 
 # ID файлів на Google Drive
 ORDERS_HEADER_ID = "1Ws7rL1uyWcYbLeXsmqmaijt98Gxo6k3i" 
